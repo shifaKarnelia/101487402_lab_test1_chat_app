eebddedfce780cbe4197b2129cb6167e5ad9f262
shifa <shifa.karnelia@georgebrown.ca>
2026-02-11 18:08:54 -0500

    final

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
